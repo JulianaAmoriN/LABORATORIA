78cbca7c1dc1504c53aec3f041cd8517983db053
JulianaAmoriN <juliana.nascimento@pravaler.com.br>
2021-09-14 13:12:25 -0300

    refatorando separando as funçoes

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+// import {provaVestibulinho} from "./perguntas";
+
 let nome = prompt("Qual seu nome?");
-if (nome == null || "") { nome = "Anônimo"};
+if (nome == null || " ") {
+        nome = "Anônimo"
+};
 document.getElementById("nome").textContent = "Olá, " + nome + " !";
 
 function participarVestibulinho() {
@@ -8,47 +12,15 @@ function participarVestibulinho() {
         caseResposta(resposta);
 }
 
-participarVestibulinho()
+participarVestibulinho();
 
 function caseResposta(resposta) {
         switch (resposta) {
-                case 'sim':
+                case "sim":
                         alert("Legal, vamos começar");
-                        const pergunta1 = prompt("Quanto é 5 X 7?\na:30\nb:35\nc:40");
-                        if (pergunta1 == "b" || pergunta1 == 35) {
-                                document.getElementById("resposta1certa")
-                                        .textContent = "Pergunta 1";
-
-                        } else {
-                                (pergunta1 == "a" || pergunta1 == 30 || pergunta1 == "c" || pergunta == "40");
-                                document.getElementById("resposta1errada")
-                                        .textContent = "Pergunta 1";
-
-                        }
-                        const pergunta2 = prompt("Quanto é 100 + 50?\na:150\nb:350\nc:400");
-                        if (pergunta2 == "a" || pergunta2 == 150) {
-                                document.getElementById("resposta2certa")
-                                        .textContent = "Pergunta 2";
-
-
-                        } else {
-                                (pergunta2 == "b" || pergunta2 == 350 || pergunta2 == "c" || pegunta == "400");
-                                document.getElementById("resposta2errada")
-                                        .textContent = "Pergunta 2";
-
-                        }
-                        const pergunta3 = prompt("Quanto é 200 - 40?\na:160\nb:100\nc:130");
-                        if (pergunta3 == "a" || pergunta3 == 160) {
-                                document.getElementById("resposta3certa")
-                                        .textContent = "Pergunta 3";
-
-                        } else {
-                                (pergunta3 == "b" || pergunta3 == "100" || pergunta3 == "c" || pergunta == "130");
-                                document.getElementById("resposta3errada")
-                                        .textContent = "Pergunta 3";
-                        }
+                        provaVestibulinho();
                         break;
-                case 'não':
+                case "não":
                         alert(" Tchauzinho, até a próxima.");
                         break;
                 default:
@@ -56,3 +28,61 @@ function caseResposta(resposta) {
                         participarVestibulinho();
         }
 }
+
+function provaVestibulinho() {
+        const pergunta1 = prompt("Quanto é 5 X 7?\na:30\nb:35\nc:40");
+        const pergunta2 = prompt("Quanto é 100 + 50?\na:150\nb:350\nc:400");
+        const pergunta3 = prompt("Quanto é 200 - 40?\na:160\nb:100\nc:130");
+
+        correcaoResposta1(pergunta1);
+        correcaoResposta2(pergunta2);
+        correcaoResposta3(pergunta3);
+}
+
+function correcaoResposta1(pergunta1) {
+        if (pergunta1 == "b" || pergunta1 == 35) {
+                pergunta1 = "certa";
+        }
+
+        switch (pergunta1) {
+                case "certa" :
+                        document.getElementById("resposta1certa")
+                        .textContent = "Pergunta 1";
+                  break;
+                default:
+                        document.getElementById("resposta1errada")
+                        .textContent = "Pergunta 1";
+        }
+}
+
+function correcaoResposta2(pergunta2) {
+
+        if (pergunta2 === "a" || pergunta2 == 150) {
+                pergunta2 = "certa";
+        }
+        switch (pergunta2) {
+                case "certa" :
+                        document.getElementById("resposta2certa")
+                        .textContent = "Pergunta 2";
+                  break;
+                default:
+                        document.getElementById("resposta2errada")
+                        .textContent = "Pergunta 2";
+        }
+}
+
+function correcaoResposta3(pergunta3) {
+        if (pergunta3 === "a" || pergunta3 == 160) {
+                pergunta3 = "certa";
+        }
+        switch (pergunta3) {
+                case "certa" :
+                        document.getElementById("resposta3certa")
+                        .textContent = "Pergunta 3";
+                  break;
+                default:
+                        document.getElementById("resposta3errada")
+                        .textContent = "Pergunta 3";
+        }
+}
+
